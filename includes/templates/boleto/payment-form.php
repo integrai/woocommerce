27b78a750c9febf6ca4efbd231afe0d0f174b0aa
Eric Barbosa <ericcarmobarbosa@gmail.com>
2021-06-08 20:22:27 -0300

    validating data for output on boleto views

--- includes/templates/boleto/payment-form.php
+++ includes/templates/boleto/payment-form.php
@@ -1,14 +1,15 @@
 <?php if (!defined('ABSPATH')) { exit; }?>
 
+<?php
+    $jsonOptions  = json_encode( isset($options) && !empty($options) ? $options : array() );
+    $jsonCustomer = json_encode( isset($customer) && !empty($customer) ? $customer : array() );
+?>
+
 <div class="form-list" id="payment_form_integrai-boleto">
     <div id="integrai-payment-boleto"></div>
 </div>
 
 <script>
-    <?php
-        $jsonOptions  = json_encode( isset($options) && !empty($options) ? $options : array() );
-        $jsonCustomer = json_encode( isset($customer) && !empty($customer) ? $customer : array() );
-    ?>
 
     window.integraiBoletoData = JSON.parse('<?php echo $jsonOptions ?>');
 
