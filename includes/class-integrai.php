ad59fa48b44a69737b10976f6862bd2f9398e94b
Eric Barbosa <ericcarmobarbosa@gmail.com>
2021-07-06 18:42:38 -0300

    release: bump version to 1.0.8

--- includes/class-integrai.php
+++ includes/class-integrai.php
@@ -28,7 +28,7 @@ class Integrai {
 		if ( defined( 'INTEGRAI_VERSION' ) ) {
 			$this->version = INTEGRAI_VERSION;
 		} else {
-			$this->version = '1.0.5';
+			$this->version = '1.0.8';
 		}
 		$this->Integrai = 'integrai';
 
