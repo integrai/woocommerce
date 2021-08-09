a6bb3d43f0c652077632b1aac10c909c3896d091
Bruno Oliveira <321.bruno@gmail.com>
2021-08-09 18:23:37 -0300

    release: bump version to 1.0.11

--- includes/class-integrai.php
+++ includes/class-integrai.php
@@ -28,7 +28,7 @@ class Integrai {
 		if ( defined( 'INTEGRAI_VERSION' ) ) {
 			$this->version = INTEGRAI_VERSION;
 		} else {
-			$this->version = '1.0.10';
+			$this->version = '1.0.11';
 		}
 		$this->Integrai = 'integrai';
 
