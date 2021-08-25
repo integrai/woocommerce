ece7488109da13a6e0a7df42aa68cf3923b98670
Bruno Oliveira <321.bruno@gmail.com>
2021-08-25 18:49:08 -0300

    add timeout for process events running

--- includes/class-integrai.php
+++ includes/class-integrai.php
@@ -28,7 +28,7 @@ class Integrai {
 		if ( defined( 'INTEGRAI_VERSION' ) ) {
 			$this->version = INTEGRAI_VERSION;
 		} else {
-			$this->version = '1.0.14';
+			$this->version = '1.0.15';
 		}
 		$this->Integrai = 'integrai';
 
